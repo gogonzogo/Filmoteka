be72501f96937f9f535901c4435b1c3e5e52315e
itsjenm <jmartinez414@gmail.com>
2023-04-14 17:38:12 -0500

    movie card rendering

--- src/js/API-requests.js
+++ src/js/API-requests.js
@@ -3,10 +3,9 @@ import { API_KEY, BASE_URL, TRENDING_PATH, MOVIE_SEARCHES, MOVIE_INFO } from './
 
 
 // HTTP Request for trending films - per day
-async function fetchTrendingMovies() {
-    
+export async function fetchTrendingMovies(page) {
     try {
-        const response = await axios.get(`${BASE_URL}${TRENDING_PATH}?api_key=${API_KEY}`);
+        const response = await axios.get(`${BASE_URL}${TRENDING_PATH}?api_key=${API_KEY}&page=${page}`);
         console.log(response.data.results)
         return response;
         
@@ -17,7 +16,7 @@ async function fetchTrendingMovies() {
 
 
 // HTTP Request for movies by keyword search
-async function getSearchMovies() {
+export async function getSearchMovies() {
     try {
         const response = await axios. get(`${BASE_URL}${MOVIE_SEARCHES}?api_key=${API_KEY}`);
         // console.log(response.data.results);
@@ -29,7 +28,7 @@ async function getSearchMovies() {
 
 
 // HTTP Request for movie information by movie Id 
-async function getMovieInfo(movie_id) {
+export async function getMovieInfo(movie_id) {
     try {
         const response = await axios.get(`${BASE_URL}${MOVIE_INFO}${movie_id}?api_key=${API_KEY}&language=en-US`);
         console.log(response);
@@ -43,8 +42,3 @@ async function getMovieInfo(movie_id) {
 // getMovieInfo(700391);
 
 
-export default {
-    fetchTrendingMovies() { console.log('trending movies') },
-    getSearchMovies() { console.log('search movies')},
-    getMovieInfo() { console.log('Movie info')}
-}
---
+++ src/js/movie-gallery.js
@@ -0,0 +1,49 @@
+import { fetchTrendingMovies, getMovieInfo} from './API-requests.js';
+
+
+
+// DOM elements
+const movieList = document.querySelector(".card-gallery");
+const LOCALSTORAGE_KEY = {};
+
+
+
+// fuction that returns trending movies 
+const showTrendingMovies = async () => {
+    const list = await fetchTrendingMovies(1);
+    console.log(list.data.results)
+    const movie = list.data.results;
+    movie.map((element) => {
+        console.log(element)
+        
+    })
+    renderInfo(movie)
+}
+
+
+showTrendingMovies();
+
+// function that renders movie info to the dom
+function renderInfo(movies) {
+    return movies.map((movie) => {
+        const card = document.createElement("div.movie-card")
+            card.classList.add('movie-card');
+            card.innerHTML = 
+                `<img src="https://image.tmdb.org/t/p/original/${movie.poster_path}" class="movie-poster"/>
+                <ul class="movie_info">
+                    <li class="movie_title">
+                        ${movie.title}
+                    </li>
+                    <li class="movie_genre">
+                        ${Object.values(movie.genre_ids)};
+                    </li>
+                    <li class="movie_release-date">
+                        ${new Date(movie.release_date).getFullYear()}
+                    </li>
+                </ul>`;
+        movieList.appendChild(card);
+    });
+}
+// const showMovieCard = (movies) => {
+//     const 
+// }
