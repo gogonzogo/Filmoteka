e7b678a1b07f7a850f860d396dff3f6e1451466a
jiml268 <jimlynch268@gmail.com>
2023-04-20 22:05:17 -0400

    set up local storeage
    model windows buttons will now add and remove items from local storage.  buttons have ben updates to change color when added or removed

--- src/js/movie-modal.js
+++ src/js/movie-modal.js
@@ -1,12 +1,39 @@
 import { getMovieInfo } from './API-requests.js';
-const movieGallery = document.querySelector('.card-gallery');
 
+class Queue {
+  constructor(id, title, genres, release_date, poster_path) {
+    this.id = id;
+    this.title = title;
+    this.genres = genres;
+    this.release_date = release_date;
+    this.poster_path = poster_path;
+  }
+}
+
+const movieGallery = document.querySelector('.card-gallery');
+let mylibID = ""
+let mylibtitle = ""
+let mylibgenres = ""
+let mylibrelease_date = ""
+let mylibposter_path = ""
+let checkArrary = -1;
+let checkclass;
 movieGallery.addEventListener('click', e => {
   //get movie id
   const parent = e.target.closest('div');
   const movieId = parent.getAttribute('data');
+  mylibID = movieId;
   renderModal(movieId);
 });
+
+let watchArray = [];
+let queueArray = [];
+let currentMovie;
+const movieWatched = document.querySelector('#watchedButton');
+const movieQueued = document.querySelector('#queuedButton');
+movieWatched.addEventListener('click', watchedProcess)  
+movieQueued.addEventListener('click', queueProcess)
+
 
 async function renderModal(movieID) {
   const modal = document.querySelector('.movie-modal__overlay');
@@ -49,4 +76,142 @@ async function renderModal(movieID) {
   `;
 
   movieAbout.textContent = movie.overview;
+
+  mylibtitle = movie.title
+mylibgenres = movie.genres.map(gen => ' ' + gen.name)
+mylibrelease_date = movie.release_date
+mylibposter_path = movie.poster_path
+ 
+  const watchSet = (localStorage.getItem('watch') !== null);
+  if (watchSet) {
+    const getWatch = localStorage.getItem('watch')
+    watchArray = JSON.parse(getWatch)
+  
+  }else {
+    watchArray = []
+  }
+    const queueSet = (localStorage.getItem('queue') !== null);
+
+  if (queueSet) {
+    const getqueue = localStorage.getItem('queue')
+    queueArray = JSON.parse(getqueue)
+  } else {
+    queueArray = []
+  }
+  const watchresult = watchArray.findIndex(item=> item.id === mylibID )
+  if (watchresult !== -1) {
+    movieWatched.innerText = "REMOVE FROM WATCHED"
+    checkclass = movieWatched.matches('.add-to-queue');
+    if (checkclass) {
+      movieWatched.classList.toggle("add-to-queue");
+      movieWatched.classList.toggle("add-to-watched");
+    }
+  } else {
+    movieWatched.innerText = "ADD TO WATCHED"
+    checkclass = movieWatched.matches('.add-to-watched');
+
+    if (checkclass) {
+      movieWatched.classList.toggle("add-to-watched");
+      movieWatched.classList.toggle("add-to-queue");
+      
+    }
+  } ;
+
+  const queueresult = queueArray.findIndex(item=> item.id === mylibID )
+ 
+  if (queueresult !== -1) {
+    movieQueued.innerText = "REMOVE FROM QUEUE" 
+    checkclass = movieQueued.matches('.add-to-queue');
+    if (checkclass) {
+      movieQueued.classList.toggle("add-to-queue");
+      movieQueued.classList.toggle("add-to-watched");
+    }
+   } else {
+    movieQueued.innerText = "ADD TO QUEUE"
+     checkclass = movieQueued.matches('.add-to-watched');
+
+    if (checkclass) {
+      movieQueued.classList.toggle("add-to-queue");
+      movieQueued.classList.toggle("add-to-watched");
+    }
+
+  };
+
 }
+
+
+function queueProcess() {
+  const queueSet = (localStorage.getItem('queue') !== null);
+   if (queueSet) {
+    const getqueue = localStorage.getItem('queue')
+   queueArray = JSON.parse(getqueue)
+     
+
+   } else {
+     queueArray = []
+   }
+
+  checkArrary = queueArray.findIndex(item=> item.id === mylibID )
+  if (checkArrary === -1) {
+   const queue = new Queue(mylibID, mylibtitle, mylibgenres, mylibrelease_date, mylibposter_path)
+queueArray.push(queue)
+    localStorage.setItem('queue', JSON.stringify(queueArray))
+    movieQueued.innerText = "REMOVE FROM QUEUE"
+    checkclass = movieQueued.matches('.add-to-queue');
+    if (checkclass) {
+      movieQueued.classList.toggle("add-to-queue");
+      movieQueued.classList.toggle("add-to-watched");
+    }
+  
+  } else {
+     queueArray.splice(checkArrary, 1)
+        localStorage.setItem('queue', JSON.stringify(queueArray))
+    movieQueued.innerText = "ADD TO QUEUE"
+     checkclass = movieQueued.matches('.add-to-watched');
+
+    if (checkclass) {
+      movieQueued.classList.toggle("add-to-queue");
+      movieQueued.classList.toggle("add-to-watched");
+    }
+      }
+    }
+  
+
+function watchedProcess() {
+     const queueSet = (localStorage.getItem('watch') !== null);
+   if (queueSet) {
+    const getwatch = localStorage.getItem('watch')
+   watchArray = JSON.parse(getwatch)
+     
+
+   } else {
+     watchArray = []
+   }
+console.log(watchArray)
+  checkArrary = watchArray.findIndex(item => item.id === mylibID)
+  console.log(checkArrary)
+  if (checkArrary === -1) {
+    const queue = new Queue(mylibID, mylibtitle, mylibgenres, mylibrelease_date, mylibposter_path)
+    console.log
+watchArray.push(queue)
+    localStorage.setItem('watch', JSON.stringify(watchArray))
+    movieWatched.innerText = "REMOVE FROM WATCHED"
+checkclass = movieWatched.matches('.add-to-queue');
+    if (checkclass) {
+      movieWatched.classList.toggle("add-to-queue");
+      movieWatched.classList.toggle("add-to-watched");
+    }
+    
+  
+  } else {
+     watchArray.splice(checkArrary, 1)
+        localStorage.setItem('watch', JSON.stringify(watchArray))
+    movieWatched.innerText = "ADD TO WATCHED"
+     checkclass = movieWatched.matches('.add-to-watched');
+
+    if (checkclass) {
+      movieWatched.classList.toggle("add-to-queue");
+      movieWatched.classList.toggle("add-to-watched");
+    }
+      }
+    }
